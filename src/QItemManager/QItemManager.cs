0e2c653c19a9ee71715fd422b0f059fcc0c82503
BeerHunter <beerhunter1337@gmail.com>
2018-04-05 23:14:29 +0300

    Init commit

---
+++ src/QItemManager/QItemManager.cs
@@ -0,0 +1,98 @@
+﻿using PoeHUD.Framework;
+using PoeHUD.Plugins;
+using PoeHUD.Poe.Components;
+using PoeHUD.Poe.Elements;
+using QItemManager.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace QItemManager
+{
+    internal class QItemManager : BaseSettingsPlugin<Settings>
+    {
+        public QItemManager()
+        {
+            PluginName = "Quality Item Manager";
+        }
+
+        public override void Render()
+        {
+            if (WinApi.IsKeyDown(Settings.DropKey))
+            {
+                var set = ScanStash();
+                if(set != null)
+                {
+                    DropToInventory(set);
+                }
+            }
+        }
+
+        public NormalInventoryItem[] ScanStash()
+        {
+            var stashPanel = GameController.Game.IngameState.ServerData.StashPanel;
+
+            if (stashPanel == null)
+            {
+                LogMessage("ServerData.StashPanel is null", 3);
+                return null;
+            }
+
+            var gems = new List<NormalInventoryItem>();
+
+            foreach (var item in stashPanel.VisibleStash.VisibleInventoryItems)
+            {
+                if (item == null)
+                {
+                    continue;
+                }
+
+                if (item.Item.HasComponent<SkillGem>() && item.Item.HasComponent<Quality>())
+                {
+                    gems.Add(item);
+                }
+            }
+
+            var sum = 0;
+            var set = new List<NormalInventoryItem>();
+            foreach(var gem in gems)
+            {
+                if (sum + gem.Item.GetComponent<Quality>().ItemQuality == 40 || sum + gem.Item.GetComponent<Quality>().ItemQuality <=35)
+                {
+                    set.Add(gem);
+                    sum += gem.Item.GetComponent<Quality>().ItemQuality;
+                }
+                if(sum == 40)
+                {
+                    break;
+                }
+            }
+            if (sum == 40)
+                return set.ToArray();
+            return null;
+        }
+
+        private void DropToInventory(IEnumerable<NormalInventoryItem> items)
+        {
+            if (items == null)
+                return;
+
+            if (!items.Any())
+                return;
+
+            var gameWindowPos = GameController.Window.GetWindowRectangle();
+            var latency = (int)GameController.Game.IngameState.CurLatency;
+
+            Keyboard.KeyDown(Keys.LControlKey);
+            Thread.Sleep(latency + Settings.ExtraDelay);
+
+            foreach (var item in items)
+            {
+                Mouse.SetCursorPosAndLeftClick(item.GetClientRect().Center + gameWindowPos.TopLeft, Settings.ExtraDelay);
+                Thread.Sleep(latency + Settings.ExtraDelay);
+            }
+        }
+
+    }
+}
---
+++ src/QItemManager/Settings.cs
@@ -0,0 +1,22 @@
+﻿using PoeHUD.Hud.Settings;
+using PoeHUD.Plugins;
+using System.Windows.Forms;
+
+namespace QItemManager
+{
+    class Settings: SettingsBase
+    {
+        public Settings()
+        {
+            Enable = false;
+            DropKey = Keys.F6;
+            ExtraDelay = new RangeNode<int>(50, 0, 2000);
+        }
+
+        [Menu("Drop Key")]
+        public HotkeyNode DropKey { get; private set; }
+
+        [Menu("Extra Click Delay")]
+        public RangeNode<int> ExtraDelay { get; set; }
+    }
+}
